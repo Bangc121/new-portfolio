3705fe914d790967b9baa8b84419dea850154641
Bangc121 <wjdrlalf17@gmail.com>
2024-06-07 18:03:47 +0900

    avatar component update

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -11,11 +11,18 @@ export default function Avatar({
   return (
     <div>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
+      {/* <img
         className={`bg-white object-cover rounded-full w-[242px] h-[242px]`}
         alt="user profile"
         src={"/images/test.jpg"}
         referrerPolicy="no-referrer" // x박스 이슈 해결
+      /> */}
+      <Image
+        src={"/images/test.jpg"}
+        alt="img"
+        priority
+        width={100}
+        height={100}
       />
     </div>
   );
